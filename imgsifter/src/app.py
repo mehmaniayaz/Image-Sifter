e31e7c1a7b94db23b6a285dbe89420761e35889b
Ayaz Mehmani <mehmani.ayaz@gmail.com>
2020-09-16 21:59:34 -0500

    making img source file name as a variable in HTML

--- imgsifter/src/app.py
+++ imgsifter/src/app.py
@@ -5,4 +5,5 @@ app = Flask(__name__,template_folder='../templates')
 
 @app.route('/')
 def home():
-    return render_template('sample_page.html')
+    ## read images from each category and display them 20 at a time. See example below.
+    return render_template('sample_page.html',img_name='/data/simplifed-data-only-oranges/1/IMG_6047.png')
