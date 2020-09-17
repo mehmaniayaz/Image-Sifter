7f4b076cec1bad3f0452912f72710eb06105a95e
Ayaz Mehmani <mehmani.ayaz@gmail.com>
2020-09-17 18:58:06 -0500

    change file name in template - jinja

--- imgsifter/src/app.py
+++ imgsifter/src/app.py
@@ -1,14 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,redirect
 import pandas as pd
 
 app = Flask(__name__,template_folder='../templates')
 df = pd.read_csv('imgsifter/src/static/data/df_classes.csv')
+df.set_index('image_name',inplace=True)
+df['class'] = df['class'].astype('string')
 
 @app.route('/')
 def home():
     ## read images from each category and display them 20 at a time. See example below.
-    return render_template('sample_page.html',img_name='/data/simplifed-data-only-oranges/1/IMG_6047.png',
-        df=df)
-@app.route('/add/1',methods=['GET'])
-def add_1():
-    return 
+    return render_template('sample_page.html',
+    img_name='/data/simplifed-data-only-oranges/1/IMG_6047.png',
+     df=df)
+@app.route('/add/2',methods=['GET'])
+def add_2():
+    df.loc['IMG_6047.png','class']='2'
+    return redirect('/')
