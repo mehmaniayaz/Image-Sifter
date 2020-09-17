4f1213da5cf1f27ea1bc3dbedbaeab93b4d27a70
Ayaz Mehmani <mehmani.ayaz@gmail.com>
2020-09-17 18:08:45 -0500

    some updates

--- imgsifter/src/app.py
+++ imgsifter/src/app.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__,template_folder='../templates')
-
+df = pd.read_csv('imgsifter/src/static/data/df_classes.csv')
 
 @app.route('/')
 def home():
     ## read images from each category and display them 20 at a time. See example below.
-    return render_template('sample_page.html',img_name='/data/simplifed-data-only-oranges/1/IMG_6047.png')
+    return render_template('sample_page.html',img_name='/data/simplifed-data-only-oranges/1/IMG_6047.png',
+        df=df)
+@app.route('/add/1',methods=['GET'])
+def add_1():
+    return 
