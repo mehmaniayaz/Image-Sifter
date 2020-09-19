b4a2e93d4e15cfed5d29a60ac23ab56dff51a901
Ayaz Mehmani <mehmani.ayaz@gmail.com>
2020-09-19 15:57:27 -0500

    updates to arrangement

--- imgsifter/src/app.py
+++ imgsifter/src/app.py
@@ -11,7 +11,7 @@ def home():
     ## read images from each category and display them 20 at a time. See example below.
     return render_template('sample_page.html',
     img_name='/data/simplifed-data-only-oranges/1/IMG_6047.png',
-     df=df)
+     df=df,ip=0)
 @app.route('/add/<string:i>',methods=['GET'])
 def change_df(i=None):
     df.loc['IMG_6047.png','class']=i
