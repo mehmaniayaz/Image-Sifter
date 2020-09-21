781c234dd942c5159d9161af3245b707574b2623
Ayaz Mehmani <mehmani.ayaz@gmail.com>
2020-09-21 10:32:43 -0500

    stay on random feature added

--- imgsifter/src/app.py
+++ imgsifter/src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,redirect
+from flask import Flask, render_template,redirect, request
 import pandas as pd
 import shutil
 from pathlib import Path
@@ -12,10 +12,10 @@ df_main['class'] = df_main['class'].astype('string')
 @app.route('/topClass/1')
 def home():
     df = df_main[df_main['class']=='1']
-    return render_template('sample_page.html',df=df,main_class=1)
+    return render_template('sample_page.html',df=df,main_class=1,view_status='all')
 
-@app.route('/add/<string:class_name>/<string:img_name>',methods=['GET'])
-def change_df(class_name=None,img_name=None):
+@app.route('/add/<view_status>/<class_name>/<img_name>',methods=['GET'])
+def change_df(view_status,class_name,img_name):
     orig_class = df_main.loc[img_name,'class']
     shutil.move(str(Path('imgsifter/src/static/data/simplifed-data-only-oranges'
     )/Path(df_main.loc[img_name,'class'])/Path(img_name)),str(Path(
@@ -23,19 +23,18 @@ def change_df(class_name=None,img_name=None):
     df_main.loc[img_name,'class']=class_name
     df_main.to_csv('imgsifter/src/static/data/df_classes.csv')
 
-    return redirect('/topClass/'+orig_class)
+    if 'random' in view_status:
+        return redirect('/select_randomly/'+orig_class)
+    else: 
+        return redirect('/select_all/'+orig_class)
+
 
 @app.route('/select_all/<string:main_class>',methods=['GET'])
 def select_all(main_class):
     df = df_main[df_main['class']==main_class]
-    return render_template('sample_page.html',df=df,main_class=main_class)
+    return render_template('sample_page.html',df=df,main_class=main_class,view_status='all')
 
 @app.route('/select_randomly/<string:main_class>',methods=['GET'])
 def select_randomly(main_class):
     df = df_main[df_main['class']==main_class].sample(10)
-    return render_template('sample_page.html',df=df,main_class=main_class)
-
-@app.route('/topClass/<string:main_class>',methods=['GET'])
-def change_main_class(main_class):
-    df = df_main[df_main['class']==main_class]
-    return render_template('sample_page.html',df=df,main_class=main_class)
+    return render_template('sample_page.html',df=df,main_class=main_class,view_status='random')
