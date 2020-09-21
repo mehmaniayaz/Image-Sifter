b5327dc059001de580ad5d5288a7c41f160b39e2
Ayaz Mehmani <mehmani.ayaz@gmail.com>
2020-09-20 21:06:20 -0500

    image movements made possible

--- imgsifter/src/app.py
+++ imgsifter/src/app.py
@@ -9,24 +9,27 @@ df_main.set_index('image_name',inplace=True)
 df_main['class'] = df_main['class'].astype('string')
 
 @app.route('/')
+@app.route('/topClass/1')
 def home():
     df = df_main[df_main['class']=='1']
     return render_template('sample_page.html',df=df,main_class=1)
+
 @app.route('/add/<string:class_name>/<string:img_name>',methods=['GET'])
 def change_df(class_name=None,img_name=None):
+    orig_class = df_main.loc[img_name,'class']
     shutil.move(str(Path('imgsifter/src/static/data/simplifed-data-only-oranges'
-    )/Path(df.loc[img_name,'class'])/Path(img_name)),str(Path(
+    )/Path(df_main.loc[img_name,'class'])/Path(img_name)),str(Path(
         'imgsifter/src/static/data/simplifed-data-only-oranges')/Path(class_name)))
-    df.loc[img_name,'class']=class_name
-    df.to_csv('imgsifter/src/static/data/df_classes.csv')
+    df_main.loc[img_name,'class']=class_name
+    df_main.to_csv('imgsifter/src/static/data/df_classes.csv')
 
-    return redirect('/')
+    return redirect('/topClass/'+orig_class)
 
 # @app.route('/select_all',methods=['GET'])
 
 # @app.route('/select_randomly',methods=['GET'])
 
-@app.route('/add/topClass/<string:main_class>',methods=['GET'])
+@app.route('/topClass/<string:main_class>',methods=['GET'])
 def change_main_class(main_class):
     df = df_main[df_main['class']==main_class]
     return render_template('sample_page.html',df=df,main_class=main_class)
